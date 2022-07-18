b0dd326b31eb9deb6f65e284600c109c04a378e8
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-18 17:08:13 +0300

    Create 1st.cpp

---
+++ 1089/1st.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector <int>* result = new vector<int>;
+        for(int i=0 ; i<arr.size() ; i++)
+        {
+            if(arr[i] == 0)
+            {
+                for(int j = arr.size()-1 ; j > i ;j--)
+                {                    
+                    arr[j] = arr[j-1];
+                }
+                i++;
+            }
+        }
+    }
+};
