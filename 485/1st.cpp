def0e4d43ccca70bd129b038b088c72ba17613ae
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-18 17:02:18 +0300

    Create 1st.cpp

---
+++ 485/1st.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
+        int result = 0;
+    int max = result;
+    
+    for(int i=0; i<nums.size() ; i)
+    {
+        if (nums[i] == 1 )
+        {
+            result++;
+            i++;
+        }
+        else
+        {
+            if(result > max) max = result;
+            result = 0;
+            i++;
+        }
+    }
+    if(result > max) max = result;
+    return max;
+    }
+};
