db22a31a3cfb44ade014ba697eb83ef41645ceea
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-27 06:11:59 +0300

    Time: 201 ms (76.94%), Space: 83 MB (77.72%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) { 
-        int i;
-        for( i=0; i<nums.size(); i++)
+        int i= 0;
+        while(i < nums.size())
         {
             if(nums[i] <= 0 || nums[i] > nums.size())
             {
                 //cout <<nums[i] <<"  changed to  0" << endl;
                 nums[i]= 0;
             }
+            i++;
         }
+        
         i= 0;
         while(i < nums.size())
         {
@@ -20,8 +22,13 @@ public:
             }       
             else i++;
         }
-
-        for(i=0 ; i<nums.size() ; i++) if(nums[i] != i+1) return i+1;
+        
+        i= 0;
+        while(i < nums.size())
+        {
+            if(nums[i] != i+1) return i+1;
+            i++;
+        }
         return nums.size()+1;
     }
     
