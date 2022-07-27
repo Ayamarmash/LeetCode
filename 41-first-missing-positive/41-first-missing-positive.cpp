03a0480f6d9cb67081dcd343c0652ad81cfc5e87
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-27 04:33:48 +0300

    Time: 258 ms (50.93%), Space: 82.9 MB (77.72%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-
-        int i;
-        int missing= 1;
-        for( i=0; i<nums.size(); i++)
+        int n=nums.size();
+        for(int i=0 ; i<n ; i++)
         {
-            if(nums[i] <= 0 || nums[i] > nums.size())
-            {
-                //cout <<nums[i] <<"  changed to  0" << endl;
-                nums[i]= 0;
-            }
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]) swap(nums[i],nums[nums[i]-1]);
         }
-        i= 0;
-        while(i < nums.size())
-        {
-            if(nums[i] != 0 && nums[i] != nums[nums[i]-1]) 
-            {
-                //cout << nums[i] << " swapped with  " << nums[nums[i]-1]<<endl;
-                Swap(nums, nums[i]- 1, i);
-            }       
-            else i++;
-        }
-
-        for(i= 0; i<nums.size(); i++)
-        {
-            cout << nums[i] << "  ,  " ;
-            if(nums[i] != 0 && nums[i] == missing)
-            { 
-                missing++;
-            }
-        }
-        //cout << endl;
-        return missing;
-}
+        for(int i=0 ; i<n ; i++) if(nums[i] != i+1) return i+1;
+        return n+1;
+    }
+    
     void Swap(vector<int>& nums, int x, int y)
     {
         int temp= nums[x];
