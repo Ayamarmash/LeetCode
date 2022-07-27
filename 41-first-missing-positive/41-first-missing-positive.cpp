d80094059e7f04c524c77c96a4093e0a43c65700
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-27 06:16:03 +0300

    Time: 192 ms (81.75%), Space: 83.1 MB (41.35%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -15,10 +15,9 @@ public:
         i= 0;
         while(i < nums.size())
         {
-            if(nums[i] != 0 && nums[i] != nums[nums[i]-1]) 
+            if(nums[i] > 0 && nums[i] != nums[nums[i]-1]) 
             {
                 //cout << nums[i] << " swapped with  " << nums[nums[i]-1]<<endl;
-                //Swap(nums, nums[i]- 1, i);
                 int temp= nums[nums[i]-1];
                 nums[nums[i]-1] = nums[i];
                 nums[i]= temp;
@@ -33,12 +32,5 @@ public:
             i++;
         }
         return nums.size()+1;
-    }
-    
-    void Swap(vector<int>& nums, int x, int y)
-    {
-        int temp= nums[x];
-        nums[x]= nums[y];
-        nums[y]= temp;
     }
 };
