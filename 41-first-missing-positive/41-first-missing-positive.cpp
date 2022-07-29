132e768840cef3714f40e902a3c1782493b8bdbc
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-29 15:41:24 +0300

    Time: 219 ms (67.81%), Space: 82.9 MB (96.09%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
-        for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0)nums[abs(nums[i]) - 1] *= -1;
-        for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
-        return n + 1;
+        int n=nums.size();
+        for(int i=0 ; i<n ; i++)
+        {
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]) 
+                swap(nums[i],nums[nums[i]-1]);
+        }
+        for(int i=0 ; i<n ; i++) if(nums[i] != i+1) return i+1;
+        return n+1;
+    }
+    
+    void Swap(vector<int>& nums, int x, int y)
+    {
+        int temp= nums[x];
+        nums[x]= nums[y];
+        nums[y]= temp;
     }
 };
