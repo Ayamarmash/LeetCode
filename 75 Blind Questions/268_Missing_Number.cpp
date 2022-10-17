440750bd7f1507236a6cfc684e650ca1778bbada
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-17 05:24:51 +0300

    Create 268_Missing_Number.cpp

---
+++ 75 Blind Questions/268_Missing_Number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        if(nums[0] != 0) return 0;
+        int i= 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] != i) return i;
+        }
+        return nums.size();
+    }
+};
+/*class Solution {
+public:
+    int missingNumber(vector<int>& nums){
+    int i = 0;
+    while (i < nums.size()) {
+      if (nums[i] < nums.size() && nums[i] != nums[nums[i]]) {
+        swap(nums, i, nums[i]);
+      } else {
+        i++;
+      }
+    }
+
+    // find the first number missing from its index, that will be our required number
+    for (i = 0; i < nums.size(); i++) {
+      if (nums[i] != i) {
+        return i;
+      }
+    }
+
+    return nums.size();
+  }
+      static void swap(vector<int> &arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+};
+*/
