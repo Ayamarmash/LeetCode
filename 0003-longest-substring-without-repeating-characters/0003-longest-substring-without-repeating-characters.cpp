8688e5e8a897190b052eee3363362573c5af8bb8
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-24 23:03:16 +0300

    Time: 37 ms (49.33%), Space: 8.3 MB (61.00%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,30 @@
+#include <algorithm>
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> freq;
-        int Start = 0;
-        int End = 0;
-        int max = 0;
-        for(End; End<s.length(); End++)
-        {                
-            freq[s[End]]++;
-            if(freq[s[End]] == 1)
-            {
-                if(max < (End-Start)+1) max = End-Start+1;
+        if(s.size() == 0 || s.size() == 1) return s.size();
+        if(s.size() == 2){
+            if(s[0] == s[1]) return 1;
+            else return 2;
+        } 
+        int start = 0;
+        int end = 0;
+        int ans = 0;
+        unordered_map<char,int> index;
+        for(end = 0; end<s.size(); end++) index[s[end]] = -1;
+        for(end = 0; end < s.size(); end++){
+            if(index[s[end]] >= 0){
+                // the char was found somewhere else, which means it's duplicated
+                if(index[s[end]] + 1 <= end) start = max(start,index[s[end]] + 1);
+                index[s[end]] = max(index[s[end]] , end);
+                ans = max(ans , end-start+1);
             }
-            else
-            {
-                while(freq[s[End]] > 1)
-                {                
-                    freq[s[Start]]--;
-                    Start++;
-                }
-                if(max < (End-Start)+1) max = End-Start+1;
+            else{
+                index[s[end]] = end;
+                ans = max(ans , end-start+1);
             }
         }
-        return max;
+
+        return ans;
     }
 };
-//// 
