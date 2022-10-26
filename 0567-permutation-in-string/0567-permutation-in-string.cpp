bc945fb2c527ade693a0ae6fb9c06eee5f6c3438
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-26 04:21:07 +0300

    Time: 12 ms (73.90%), Space: 8.1 MB (23.15%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,15 +4,15 @@ public:
     {
         unordered_map <char, int> ptrn;
         unordered_map <char, int> str;
-        int i;
-        int start = 0, end = pattern.length();
+
+        int start = 0, end;
         if(pattern.length() > string.length()) return false;
-        for(i=0; i<pattern.length(); i++) {
-            ptrn[pattern[i]]++; 
-            str[string[i]]++;
+        for(end=0; end<pattern.length(); end++) {
+            ptrn[pattern[end]]++; 
+            str[string[end]]++;
         }
 
-        for(end; end<string.length(); end++){
+        for(end = pattern.length(); end<string.length(); end++){
             if(ptrn == str) return true;
             str[string[start]]--;
             if(str[string[start]] == 0) str.erase(string[start]);
