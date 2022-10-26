d8427aaf3086949a07625ef21e3740e290538bd1
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-26 04:17:51 +0300

    Time: 128 ms (12.26%), Space: 12.9 MB (11.78%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -8,17 +8,19 @@ public:
         int start = 0, end = p.length();
         if(p.length() > s.length()) return {};
         
-        for(end=0; end<p.length(); end++) {
-            ptrn[p[end]]++; 
-            str[s[end]]++;
-        }
-
-        for(end=p.length(); end<s.length(); end++){
-            if(ptrn == str) answer.push_back(start);
-            str[s[start]]--;
-            if(str[s[start]] == 0) str.erase(s[start]);
-            start++;
-            str[s[end]]++;
+        for(end=0; end<s.length(); end++){
+            if(end < p.length()) {
+                ptrn[p[end]]++; 
+                str[s[end]]++;
+            }
+            else 
+            {
+                if(ptrn == str) answer.push_back(start);
+                str[s[start]]--;
+                if(str[s[start]] == 0) str.erase(s[start]);
+                start++;
+                str[s[end]]++;
+            }
         }
         if(ptrn == str) answer.push_back(start);
         return answer;
