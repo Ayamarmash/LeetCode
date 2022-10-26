2183421e6eca7d4b17266ce44ce0c5a462b7bb79
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-10-26 04:15:39 +0300

    Time: 119 ms (14.28%), Space: 12.8 MB (11.78%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> answer;
         unordered_map <char, int> ptrn;
         unordered_map <char, int> str;
-        int i;
+
         int start = 0, end = p.length();
         if(p.length() > s.length()) return {};
         
-        for(i=0; i<p.length(); i++) {
-            ptrn[p[i]]++; 
-            str[s[i]]++;
+        for(end=0; end<p.length(); end++) {
+            ptrn[p[end]]++; 
+            str[s[end]]++;
         }
 
-        for(end; end<s.length(); end++){
+        for(end=p.length(); end<s.length(); end++){
             if(ptrn == str) answer.push_back(start);
             str[s[start]]--;
             if(str[s[start]] == 0) str.erase(s[start]);
