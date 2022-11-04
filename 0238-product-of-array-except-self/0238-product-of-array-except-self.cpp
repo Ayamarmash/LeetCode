bada8c62d1f95dbf6f6dd0a3af1616780a869649
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-11-04 15:13:30 +0200

    Time: 60 ms (33.12%), Space: 25 MB (40.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         if(nums.size() == 2) return {nums[1], nums[0]};
         vector<int> answer;
-        int preProduct = 1, postProduct = 1;
+        int Product = 1;
         int i;
         for(i=0; i<nums.size(); i++){
-            answer.push_back(preProduct);
-            preProduct = preProduct * nums[i];
+            answer.push_back(Product);
+            Product = Product * nums[i];
         }
+        Product = 1;
         for(i=nums.size()-1; i >= 0; i--){
-            answer[i] = answer[i] * postProduct;
-            postProduct = postProduct * nums[i];
+            answer[i] = answer[i] * Product;
+            Product = Product * nums[i];
         }
         return answer;
     }
