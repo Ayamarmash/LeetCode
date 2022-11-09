1f6512b48950cb5460cee902174283c134b49bb9
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-11-09 20:39:25 +0200

    Time: 18 ms (72.06%), Space: 7.9 MB (95.73%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,14 +11,12 @@ public:
     bool hasCycle(ListNode *head) {
         if(head == NULL) return false;
         if(head->next == NULL) return false;
-        ListNode* Ptr1 = head;
-        ListNode* Ptr2 = head->next;
-        while(Ptr2 != NULL && Ptr2->next != NULL){
-            if(Ptr1 == Ptr2) return true;
-            else{
-                Ptr1 = Ptr1->next;
-                Ptr2 = Ptr2->next->next;
-            }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL && fast->next != NULL){
+            if(slow == fast) return true;
+            slow = slow->next;
+            fast = fast->next->next;
         }
         return false;
     }
