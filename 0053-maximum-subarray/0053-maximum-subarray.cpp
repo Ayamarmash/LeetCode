cbb23ebbeb4c70255380f30fa5fe6c11059521be
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2022-12-05 09:25:05 +0200

    Time: 364 ms (10.64%), Space: 67.8 MB (55.45%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
+        if(nums.size() < 0) return 0;
         if(nums.size() == 1) return nums[0];
-        
-        int End = 0;
-        int max = nums[0];
-        int current = 0;
-        for(End; End<nums.size(); End++)
-        {
-            current += nums[End];
-            if(current > max) max = current;
-            if(current < 0) current = 0;
-
+        int i = 0, max = nums[0], currentSum = 0;
+        for(i; i < nums.size(); i++){
+            currentSum += nums[i];
+            if(currentSum > max) max = currentSum;
+            if(currentSum < 0) currentSum = 0;
         }
         return max;
     }
