6c3a5b190e6530f553d0a719474bb70825d7bfae
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2023-01-09 16:08:25 +0200

    Time: 5 ms (26.04%), Space: 6 MB (92.08%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.length() == 0) return false;
+        if(word.length() == 1) return true;
+        int start = 0;
+        if(word[start] >= 65 && word[start] <= 90) start++;
+
+        bool isCapital;
+        if(word[start] >= 65 && word[start] <= 90) isCapital = true;
+        else isCapital = false;
+
+        start++;
+        while(start < word.length()){
+            if(word[start] >= 65 && word[start] <= 90 && isCapital) start++;
+            else if (word[start] >= 97 && word[start] <= 122 && !isCapital) start++;
+            else return false;
+        }
+        return true;
+    }
+};
