15926805669493f99e85970276b67abba7209350
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2023-01-09 20:43:55 +0200

    Time: 3 ms (81.52%), Space: 8.5 MB (41.37%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         if(nums.size() == 0) return {};
-        int sumSoFar = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sumSoFar += nums[i];
-            nums[i] = sumSoFar;
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] += nums[i-1];
         }
         return nums;
     }
