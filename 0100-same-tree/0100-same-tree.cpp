1dc7c3636657fd5ef65d657668d133dc4f829bdf
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2023-01-10 02:38:53 +0200

    Time: 0 ms (100.00%), Space: 10.1 MB (8.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,14 +14,10 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
-        
-        if(p->val != q->val) return false;
-        bool right, left;
         if(p->val == q->val)
         {
-            right= isSameTree(p->right, q->right);
-            left= isSameTree(p->left, q->left);
+            return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
         }
-        return right&&left;
+        return false;
     }
 };
