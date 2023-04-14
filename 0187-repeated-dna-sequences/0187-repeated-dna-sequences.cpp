91b563afa5c20acae3c023e191fa9ef54c32c2df
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2023-04-14 06:26:28 +0200

    Time: 74 ms (43.23%), Space: 23.4 MB (38.25%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -6,9 +6,8 @@ public:
         vector<string> result;
         unordered_map<string, int> freq;
         while(start <= s.length() - 10){
-            string temp = s.substr(start, 10);
-            freq[temp]++;
-            if(freq[temp] == 2) result.push_back(temp);
+            freq[s.substr(start, 10)]++;
+            if(freq[s.substr(start, 10)] == 2) result.push_back(s.substr(start, 10));
             start++;
         }
         return result;
