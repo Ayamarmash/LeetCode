ce0f26e98e83bc59c9209cd9d458685750b1d07f
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2024-08-12 15:18:28 +0300

    Time: 51 ms (55.99%) | Memory: 49 MB (11.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -7,11 +7,15 @@ var climbStairs = function (n) {
     if (n === 0 || n === 1) return 1;
     if (n === 2) return 2;
 
-    let dp = [1, 1];
-    for (let i = 2; i <= n; i++) {
-        let temp = dp[i - 1] + dp[i - 2];
-        dp.push(temp);
+    let minOne = 1;
+    let minTwo = 2;
+    let latest = 0;
+
+    for (let i = 2; i < n; i++) {
+        latest = minOne + minTwo;
+        minOne = minTwo;
+        minTwo = latest;
     }
-    console.log(dp)
-    return dp[n];
+    
+    return latest;
 };
