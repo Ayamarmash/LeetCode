21a7154bf74b770e38345193f5628a5fd5eb9760
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2024-08-12 16:44:33 +0300

    Time: 60 ms (79.22%) | Memory: 53.4 MB (33.95%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+    if (nums.length === 1) return true;
+
+    let maxReachableIndex = 0;
+    for (let i = 0; i < nums.length; i++) {
+
+        // if we are now beyond the max reachable index ( we can't reach it )
+        if (i > maxReachableIndex) return false;
+
+        if (maxReachableIndex >= nums.length-1) return true;
+
+        // if we moved nums[i] steps from the current index i, where we will reach ?
+        if (i + nums[i] > maxReachableIndex) maxReachableIndex = i + nums[i];
+    }
+    return false;
+};
