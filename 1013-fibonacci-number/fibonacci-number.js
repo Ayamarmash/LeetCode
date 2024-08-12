b7755745bdba838c8a6dcfd18384f8ca796de9ef
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2024-08-12 14:21:13 +0300

    Time: 50 ms (78.39%) | Memory: 48.5 MB (70.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 var fib = function (n) {
-    if (n === 0) return 0;
-    if (n === 1) return 1;
-    return fib(n - 1) + fib(n - 2);
-};
+    if (n <= 1) return n;
+
+    let dp = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        let tmp = dp[i - 1] + dp[i - 2];
+        dp.push(tmp);
+    }
+
+    return dp[n];
+}
