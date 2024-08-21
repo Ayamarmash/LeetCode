873892d46e7efb751fd1852f893e7277954a9ab7
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2024-08-21 12:39:58 +0300

    Time: 53 ms (41.67%) | Memory: 48.6 MB (79.63%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
@@ -7,9 +7,6 @@ var squareIsWhite = function (coordinates) {
     let map = {
         'a': false, 'b': true, 'c': false, 'd': true, 'e': false, 'f': true, 'g': false, 'h': true
     };
-    // b --> true
-    // b2 ---> 2%2 === 0 ---------> true 
-    // true || true ---> true
     if (!map[x]) return map[x] || y % 2 === 0;
     return map[x] && y % 2 !== 0;
 };
