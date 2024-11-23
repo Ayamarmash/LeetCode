53b0ba21fd26f788a3e2ee82c789385abc0d69e7
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2024-11-23 23:11:16 +0200

    Time: 1614 ms (16.40%) | Memory: 51.8 MB (89.48%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+    if (nums.length === 1) {
+        return nums[0] === k ? 1 : 0;
+    }
+    let startWindow = 0, endWindow = 0;
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let currentSum = 0;
+        for (let j = i; j < nums.length; j++) {
+            currentSum += nums[j];
+            if (currentSum === k) res++;
+        }
+    }
+    return res;
+};
