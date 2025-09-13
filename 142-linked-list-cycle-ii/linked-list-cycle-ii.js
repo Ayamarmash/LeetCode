6670608a577d2ab441f7f5f23d6e95b3bbfd31c5
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2025-09-13 16:58:44 +0300

    Time: 61 ms (5.43%) | Memory: 56.7 MB (88.42%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+    let slow = head, fast = head;
+    let linkLen = 0;
+    if(!head || !head.next) return null;
+    
+    while (fast?.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) break;
+    }
+
+    // cycle detected
+    if (fast === slow) {
+        let ptr = head;
+        while (ptr !== slow) {
+            ptr = ptr.next;
+            slow = slow.next;
+        }
+        return ptr;
+    }
+
+    return null;
+};
