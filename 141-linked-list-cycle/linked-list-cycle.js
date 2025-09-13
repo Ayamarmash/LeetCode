b92139acad98465e2b796ab24d654353d8a8c4f4
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2025-09-13 15:52:08 +0300

    Time: 53 ms (56.91%) | Memory: 59.6 MB (23.63%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -10,14 +10,13 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    if(!head || !head.next) return false
-    let slow = head;
-    let fast = head.next;
-    while(fast && fast.next){
-        if(slow === fast) return true;
+var hasCycle = function (head) {
+    let slow = head, fast = head;
+
+    while (fast?.next) {
         slow = slow.next;
         fast = fast.next.next;
+        if(slow === fast) return true;
     }
     return false;
 };
