bb0ab399777d16dc72237a2b7fe93366bd37278c
Aya Marmash <58006991+Ayamarmash@users.noreply.github.com>
2025-09-14 19:25:50 +0300

    Time: 959 ms (5.17%) | Memory: 83.7 MB (27.23%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -10,17 +10,36 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-    let stack = [];
-    let ptr = head;
-    while (ptr) {
-        stack.push(ptr.val)
-        ptr = ptr.next
+
+    let slow = head, fast = head;
+
+    // slow will be at the middle when fast is in the end (fast moves x2)
+    while (fast?.next) {
+        slow = slow.next;
+        fast = fast.next.next
     }
-    ptr = head;
-    while (ptr) {
-        if (ptr.val !== stack[stack.length-1]) return false;
-        ptr = ptr.next;
-        stack.pop();
+    console.log(slow.val)
+    // reverse the second half of the linked list
+    let ptr = slow;
+    let current = slow.next
+    if (!current) return slow.val === head.val;
+    ptr.next = null;
+    while (current) {
+        let rest = current.next;
+        current.next = ptr;
+        ptr = current;
+        console.log(current.val)
+        if (!rest) break;
+        current = rest;
     }
+
+    let first = head;
+    while (current && first) {
+        console.log(current.val, ', ', first.val)
+        if (current.val !== first.val) return false;
+        current = current.next;
+        first = first.next;
+    }
+
     return true;
 };
